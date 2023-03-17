30491585f30b7608af89253ea8c9691c9c9b211f
pjodha <prakash.jodha@clarivate.com>
2023-03-17 15:23:11 +0530

    DB changes

--- src/main/resources/resourceFile/database.sql
+++ src/main/resources/resourceFile/database.sql
@@ -1,12 +1,176 @@
-//Add your Db queries related here
-//User_creation
-CREATE TABLE `socialplatform`.`user_creation` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(100) NULL,
-  `password` VARCHAR(50) NOT NULL,
-  `username` VARCHAR(50) NULL,
+-- MySQL dump 10.13  Distrib 8.0.32, for macos13.0 (x86_64)
+--
+-- Host: 127.0.0.1    Database: pq_social
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `comment_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  `comment_description` varchar(500) NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `comments_fk_1` (`user_id`),
+  KEY `comments_fk_2` (`post_id`),
+  CONSTRAINT `comments_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `comments_fk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_likes`
+--
+
+DROP TABLE IF EXISTS `post_likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_likes` (
+  `post_like_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  PRIMARY KEY (`post_like_id`),
+  KEY `post_likes_fk_1` (`user_id`),
+  KEY `post_likes_fk_2` (`post_id`),
+  CONSTRAINT `post_likes_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `post_likes_fk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_likes`
+--
+
+LOCK TABLES `post_likes` WRITE;
+/*!40000 ALTER TABLE `post_likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `msg` varchar(500) NOT NULL,
+  `post_type` int NOT NULL DEFAULT '0',
+  `media_url` varchar(100) NOT NULL DEFAULT '',
+  `like_count` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`post_id`),
+  KEY `posts_fk_1` (`user_id`),
+  CONSTRAINT `posts_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_relations`
+--
+
+DROP TABLE IF EXISTS `user_relations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_relations` (
+  `user_relation_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id_1` bigint NOT NULL,
+  `user_id_2` bigint NOT NULL,
+  `status` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`user_relation_id`),
+  KEY `user_relations_fk_1` (`user_id_1`),
+  KEY `user_relations_fk_2` (`user_id_2`),
+  CONSTRAINT `user_relations_fk_1` FOREIGN KEY (`user_id_1`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `user_relations_fk_2` FOREIGN KEY (`user_id_2`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_relations`
+--
+
+LOCK TABLES `user_relations` WRITE;
+/*!40000 ALTER TABLE `user_relations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_relations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(50) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
- //Insert query
- INSERT INTO `socialplatform`.`user_creation` (`user_id`, `email`, `password`, `username`)
- VALUES ('2', 'Abc@gmail.com', 'tuxvfgh23', 'Sahil');
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'pq_social'
+--
+
+--
+-- Dumping routines for database 'pq_social'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-17 15:20:50
